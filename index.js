d34c4a65e8b699b8cf4b31a557ac8018f53c4756
Mahavir Singh <44024089+bishtmanish739@users.noreply.github.com>
2022-10-07 10:21:13 +0530

    node server added

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const fs=require("fs");
+const http=require("http");
 
 console.log("Hello world");
 fs.writeFileSync("hello.txt","Hello this is node first class");
 
+http.createServer((req,res)=>{
+   
+    res.write("<h1>Hello welcome to node first class</h1>");
+    
+    res.end();
+}).listen(4500);
