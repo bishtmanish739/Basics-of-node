3fb9c8b6b174eaae2f954daf8ca461d1dcdfb9dd
Mahavir Singh <44024089+bishtmanish739@users.noreply.github.com>
2022-10-09 15:53:26 +0530

    color package

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
+// what is package.json and npm init 
+// package.json is a file which track all node module used in out project
+// it is most important file in our project
+// how to recover node.module file => using npm install command 
 const fs=require("fs");
 const http=require("http");
+const c=require("colors");
+const { inherits } = require("util");
 
-console.log("Hello world");
+console.log("Hello world".blue);
+console.log("I am color module in node".red)
 fs.writeFileSync("hello.txt","Hello this is node first class");
 
 http.createServer((req,res)=>{
