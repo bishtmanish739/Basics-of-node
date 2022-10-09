77f6465150d6c9423c37df40780a036435c1ff31
Mahavir Singh <44024089+bishtmanish739@users.noreply.github.com>
2022-10-09 16:14:17 +0530

    how to make first api in node

---
+++ apidata.js
@@ -0,0 +1,8 @@
+data={
+    
+        id:1,
+        name:"mahavir",
+        age:23
+    
+}
+module.exports=data;
--- index.js
+++ index.js
@@ -3,19 +3,14 @@
 // it is most important file in our project
 // how to recover node.module file => using npm install command 
 //nodemon is a package is used to save project and run our server contineously 
-
+/// this is comment at node api branch 
+const data=require("./apidata")
 const fs=require("fs");
 const http=require("http");
 const c=require("colors");
-const { inherits } = require("util");
-
-console.log("Hello world".blue);
-console.log("I am color module in node".red)
-fs.writeFileSync("hello.txt","Hello this is node first class");
-
 http.createServer((req,res)=>{
-   
-    res.write("<h1>Hello welcome to node first class</h1>");
+   res.writeHead(200,{'Content-Type':'application\json'});
+    res.write(JSON.stringify(data));
     
     res.end();
 }).listen(4500);
