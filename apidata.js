40d08f03aa6210ec16ca8c027888c75f3dcb2774
Mahavir Singh <44024089+bishtmanish739@users.noreply.github.com>
2022-10-09 16:20:17 +0530

    static data api

--- apidata.js
+++ apidata.js
@@ -1,8 +1,24 @@
-data={
-    
-        id:1,
-        name:"mahavir",
-        age:23
-    
-}
+data=[
+        {
+                id:1,
+                name:"Mahavir",
+                age:23
+        },
+        {
+                id:2,
+                name:"Rohit ",
+                age:22
+        },
+        {
+                id:3,
+                name:"Karan",
+                age:22
+        },
+        {
+                id:4,
+                name:"Anil",
+                age:21
+        }
+
+]
 module.exports=data;
